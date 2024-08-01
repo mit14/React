e6675cac9becdfd4e9189aa0da6da186470547a9
mit14 <mit14@live.com>
2024-08-01 19:45:31 -0400

    Section 3 exampled completed

---
+++ 3_React_Essentials/src/components/CoreConcepts.jsx
@@ -0,0 +1,12 @@
+
+
+// intead of props we can also use the destructuring. so just use the {} and pass the keys inside, so the keys here are image, title desctiption
+export default function CoreConcept({image, title, description}) {
+    return (
+      <li>
+        <img src={image} alt={title} />
+        <h3> {title} </h3>
+        <p> {description} </p>
+      </li>
+    );
+  }
